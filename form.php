acecb052bdded2f4700924da34f43b7f35452b17
soupInnerHTML <54484876+soupInnerHTML@users.noreply.github.com>
2020-08-29 12:52:06 +0500

    Delete form.php

--- form.php
+++
@@ -1,5 +0,0 @@
-$name = $_POST['user_name']; 
-$mail_to = 'andrey.da.scrim@gmail.com' 
-$header = 'Тест'; 
-$mes = "Имя: $name";
-mail($mail_to, $header, $mes, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
